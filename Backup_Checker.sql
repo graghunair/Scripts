c0e40739c33b07cb31765297c40d1d7474b51f6e
Raghu Gopalakrishnan <graghunair@gmail.com>
2023-01-17 11:37:03 +0300

    Update Backup_Checker.sql

--- Backup_Checker.sql
+++ Backup_Checker.sql
@@ -1,10 +1,12 @@
 SELECT	SERVERPROPERTY('servername') AS [Server_Name], 
 		[name] AS [Database_Name], 
         d AS [Last_Full_Backup], 
+		DATEDIFF(dd, d, GETDATE()) [Last_Full_Backup_Days_Old],
         i AS [Last _Differential_Backup], 
         l AS [Last_TLog_Backup], 
 		[recovery_model_desc] AS [Recovery Model], 
         [state_desc] AS [Database State],
+		sys.fn_hadr_backup_is_preferred_replica([name]) AS [Is_Preferred_Replica],
 		GETDATE() AS [Date_Captured]
 FROM	(SELECT			db.[name], 
 						db.[database_id], 
@@ -21,5 +23,5 @@ PIVOT
         (MAX([backup_finish_date]) FOR [type] IN (D,I,L)) AS Last_Backup 
 WHERE	[state] = 0 
 AND		[database_id] <> 2 
-ORDER BY 3,4,5 
-GO 
+ORDER BY d,i,l
+GO
